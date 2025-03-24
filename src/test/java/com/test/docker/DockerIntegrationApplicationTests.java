6e2d45bc5caa6f731ae0ead294b5bfc20542928c
chinakoti <kotijava111@gmail.com>
2025-03-24 12:18:19 +0530

    First Commit

---
+++ src/test/java/com/test/docker/DockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
